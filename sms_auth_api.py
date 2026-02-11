c2b33e3ae8cfd44422539144354a194c0b61a35f
zeroHYH <HWANG_YH@outlook.com>
2026-02-11 14:13:48 +0800

    Refactor SMS auth session management

--- sms_auth_api.py
+++ sms_auth_api.py
@@ -1,10 +1,11 @@
-from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime
 from random import random
+from uuid import UUID, uuid7
 
+import httpx
 from fastapi import Body, FastAPI, HTTPException, Request, Response
 from fastapi.staticfiles import StaticFiles
-from httpx import Client, HTTPError
+from httpx import Client, Cookies, HTTPError
 from uvicorn import run
 
 from uniform_login_des import strEnc
@@ -12,139 +13,97 @@ from uniform_login_des import strEnc
 app = FastAPI(debug=True, title="统一认证短信登录")
 
 
-@dataclass
-class LoginSession:
-    client: Client
-    expire: datetime
-
-
-login_sessions: dict[str, LoginSession] = {}
+login_sessions: dict[str, Cookies] = {}
 
 
 def clean_expired_sessions():
-    now = datetime.now()
-    expired = [c for c, ls in login_sessions.items() if now > ls.expire]
-    for c in expired:
-        login_sessions.pop(c)
+    now = datetime.now().timestamp()
+    [login_sessions.pop(u) for u in login_sessions if now > UUID(u).time + 60 * 1000]
 
 
 @app.post("/code")
-def get_image_code(request: Request):
+def get_image_code():
     clean_expired_sessions()
-    ls = request.cookies.get("login_session")
-    new_session = False
-    if not ls or ls not in login_sessions:
-        ls = str(random())
-        login_sessions[ls] = LoginSession(
-            client=Client(), expire=datetime.now() + timedelta(minutes=2)
-        )
-        new_session = True
     try:
-        upstream = login_sessions[ls].client.get(
-            f"https://pass.sdu.edu.cn/cas/code?{ls}"
-        )
+        upstream = httpx.get(f"https://pass.sdu.edu.cn/cas/code?{random()}")
     except HTTPError:
         raise HTTPException(503, "cas unavailable")
-    media_type = upstream.headers.get("content-type", "image/gif")
-    response = Response(upstream.content, media_type=media_type)
-    if new_session:
-        response.set_cookie("login_session", ls, max_age=150, httponly=True)
+    login_sessions[(ls := uuid7().hex)] = upstream.cookies
+    response = Response(upstream.content, media_type="image/gif")
+    response.set_cookie("login_session", ls, max_age=60, httponly=True)
     return response
 
 
-@app.post("/sms")
+@app.post("/sms", status_code=201)
 def get_sms_code(
     request: Request,
-    response: Response,
     mobile: str = Body(max_length=11, min_length=11, pattern=r"^\d+$"),
     code: str = Body(max_length=4, min_length=4, pattern=r"^\d+$"),
 ):
     clean_expired_sessions()
-    ls = request.cookies.get("login_session")
-    if not ls or ls not in login_sessions:
+    if (ls := request.cookies.get("login_session")) not in login_sessions:
         raise HTTPException(400, "Invalid session or session expired")
     try:
-        url = (
-            login_sessions[ls]
-            .client.post(
-                "https://pass.sdu.edu.cn/cas/loginByMorE",
-                data={
-                    "method": "sendMobileCode",
-                    "sendConfirm": code,
-                    "mobile": mobile,
-                    "random": random(),
-                },
-            )
-            .json()
-            .get("redirectUrl")
+        res = httpx.post(
+            "https://pass.sdu.edu.cn/cas/loginByMorE",
+            data={
+                "method": "sendMobileCode",
+                "sendConfirm": code,
+                "mobile": mobile,
+                "random": random(),
+            },
+            cookies=login_sessions[ls],
         )
     except HTTPError:
         raise HTTPException(503, "cas unavailable")
-    if url == "login":
-        response.status_code = 201
-    else:
+    login_sessions[ls].update(res.cookies)
+    if res.json().get("redirectUrl") != "login":
         raise HTTPException(400, "Invalid code")
 
 
 @app.post("/login")
 def sms_login(
     request: Request,
-    response: Response,
     mobile: str = Body(max_length=11, min_length=11, pattern=r"^\d+$"),
     code: str = Body(max_length=6, min_length=6, pattern=r"^\d+$"),
 ):
     clean_expired_sessions()
-    ls = request.cookies.get("login_session")
-    if not ls or ls not in login_sessions:
+    if (ls := request.cookies.get("login_session")) not in login_sessions:
         raise HTTPException(400, "Invalid session or session expired")
+    client = Client(cookies=login_sessions.pop(ls))
     try:
-        url = (
-            login_sessions[ls]
-            .client.post(
-                "https://pass.sdu.edu.cn/cas/loginByMorE",
-                data={
-                    "method": "login",
-                    "mobile": mobile,
-                    "mobileCode": code,
-                    "random": random(),
-                    "service": "https://service.sdu.edu.cn/tp_up/view?m=up",
-                },
-            )
-            .json()
-            .get("redirectUrl")
+        res = client.post(
+            "https://pass.sdu.edu.cn/cas/loginByMorE",
+            data={
+                "method": "login",
+                "mobile": mobile,
+                "mobileCode": code,
+                "random": random(),
+                "service": "https://service.sdu.edu.cn/tp_up/view?m=up",
+            },
         )
     except HTTPError:
         raise HTTPException(503, "cas unavailable")
-    if not url:
+    if not (url := res.json().get("redirectUrl")):
         raise HTTPException(400, "Invalid code")
     try:
-        login_sessions[ls].client.get(url, follow_redirects=True)
-        sduid = (
-            login_sessions[ls]
-            .client.post(
-                "https://service.sdu.edu.cn/tp_up/sys/uacm/profile/getUserType",
-                json={},
-                headers={
-                    "Content-Type": "application/json;charset=UTF-8",
-                },
-            )
-            .json()[0]["ID_NUMBER"]
-        )
-        info = (
-            login_sessions[ls]
-            .client.post(
-                "https://service.sdu.edu.cn/tp_up/sys/uacm/profile/getUserById",
-                json={"BE_OPT_ID": strEnc(sduid, "tp", "des", "param")},
-                headers={
-                    "Content-Type": "application/json;charset=UTF-8",
-                },
-            )
-            .json()
-        )
+        client.get(url, follow_redirects=True)
+        sduid = client.post(
+            "https://service.sdu.edu.cn/tp_up/sys/uacm/profile/getUserType",
+            json={},
+            headers={
+                "Content-Type": "application/json;charset=UTF-8",
+            },
+        ).json()[0]["ID_NUMBER"]
+        info = client.post(
+            "https://service.sdu.edu.cn/tp_up/sys/uacm/profile/getUserById",
+            json={"BE_OPT_ID": strEnc(sduid, "tp", "des", "param")},
+            headers={
+                "Content-Type": "application/json;charset=UTF-8",
+            },
+        ).json()
     except HTTPError:
         raise HTTPException(503, "could not get user info from service.sdu.edu.cn")
-    login_sessions.pop(ls)
-    response.delete_cookie("login_session")
     return {
         "name": info.get("USER_NAME"),
         "sduid": sduid,
